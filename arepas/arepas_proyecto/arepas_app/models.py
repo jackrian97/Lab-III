7379f951b133a8950bc17fb21357bae32289b11d
Brian <jack.rian97@hotmail.com>
2024-04-03 17:48:32 +0000

    esto es una prueba

--- arepas/arepas_proyecto/arepas_app/models.py
+++ arepas/arepas_proyecto/arepas_app/models.py
@@ -5,7 +5,7 @@ class Arepa(models.Model):  #nombre de la tabla en la Base de Datos
     nombre = models.CharField(max_length=100, default='DEFAULT VALUE')
     precio = models.CharField(max_length=20, default='DEFAULT VALUE')
     stock = models.CharField(max_length=100, default='DEFAULT VALUE')
-    img = models.FileField()
+    img = models.ImageField(upload_to='')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
